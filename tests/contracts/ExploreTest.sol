148202150c52a3d5578bbac852e67e9dd5ecee28
Amal Sudama <cds.sudama@gmail.com>
2018-09-12 13:47:51 -0400

    test for tracking transfers from contracts

--- tests/contracts/ExploreTest.sol
+++ tests/contracts/ExploreTest.sol
@@ -3,11 +3,19 @@ pragma solidity 0.4.24;
 
 contract ExploreTest {
 
+    event LogPaid(address recipient, uint256 amount);
+
     // Allow receipt of either
     function() public payable {}
 
-    function sendTo(address _address, uint256 amount) public payable {
-        _address.transfer(amount);
+    function transferTo(address recipient, uint256 amount) public payable {
+        recipient.transfer(amount);
+        emit LogPaid(recipient, amount);
+    }
+
+    function blockedTransferTo(address recipient, uint256 amount) public payable {
+        require(false);
+        recipient.transfer(amount);
     }
 
     function getBalance() public view returns(uint256) {
---
+++ tests/test/testFromContract.js
@@ -0,0 +1,38 @@
+const Actor = artifacts.require('ExploreTest')
+const { blockQuery } = require('../../src/explorer')
+
+contract('TestExplorer blockQuery', ([acc1, acc2, acc3, ...rest]) => {
+  let actor
+  const args = {
+    FROMCURRENTBLOCK: 1000,
+    '--logtrans': false,
+
+    // uncomment for `truffle develop testing`
+    '--net': 'testing'
+
+    // uncomment for testing agains local ganache
+    // '--net': 'local'
+  }
+  const oneEther = web3.toWei(1, 'ether')
+
+  before(async () => {
+    actor = await Actor.new()
+    await actor.send(oneEther, { from: acc1 })
+  })
+
+  it('should have an initial balance of 1 ether', async () => {
+    const balance = (await actor.getBalance()).toNumber(10)
+    assert.equal(oneEther, balance)
+  })
+
+  it('should detect transfer funds from contract', async () => {
+    await actor.transferTo(acc2, 9999)
+    const balance = (await actor.getBalance()).toNumber(10)
+    assert.notEqual(oneEther, balance)
+
+    const db = await blockQuery(args)
+
+    // Can transfers from contracts be detected?
+    assert.isFalse(db.totalOutContracts.isZero())
+  })
+})
